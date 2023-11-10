ac416a0a5507b2b9932829c11867ea25077832eb
yassinex02 <tahayassinemoumni@gmail.com>
2023-11-10 01:09:08 +0100

    last

---
+++ tests/test_data_load.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+from src.data_loader.data_load import load_data
+
+
+def test_load_excel_file():
+    file_path = 'tests/data/test_data.xlsx'
+    df = load_data(file_path, sheet_name='Sheet1')
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 3
+    assert list(df.columns) == ['Name', 'Age', 'Gender']
+    os.remove(file_path)
+
+
+def test_load_csv_file():
+    file_path = 'tests/data/test_data.csv'
+    df = load_data(file_path)
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 3
+    assert list(df.columns) == ['Name', 'Age', 'Gender']
+    os.remove(file_path)
+
+
+def test_load_json_file():
+    file_path = 'tests/data/test_data.json'
+    df = load_data(file_path)
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 3
+    assert list(df.columns) == ['Name', 'Age', 'Gender']
+    os.remove(file_path)
+
+
+def test_file_not_found():
+    file_path = 'tests/data/non_existent_file.xlsx'
+    try:
+        df = load_data(file_path)
+    except FileNotFoundError as e:
+        assert str(e) == f"File not found: {file_path}"
+
+
+def test_unsupported_file_format():
+    file_path = 'tests/data/test_data.txt'
+    try:
+        df = load_data(file_path)
+    except ValueError as e:
+        assert str(e) == f"Unsupported file format: .txt"
