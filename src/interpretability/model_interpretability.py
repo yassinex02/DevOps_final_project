7ede55579001660411336925af4912a649d37c11
Momo <mozeart@hotmail.com>
2023-12-11 07:24:56 +0100

    Final Final Changes

--- src/interpretability/model_interpretability.py
+++ src/interpretability/model_interpretability.py
@@ -12,15 +12,22 @@ import argparse
 import wandb
 from transformer import FactorizeTransformer
 
-
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger(__name__)
 
 
-def run_shap_analysis(
-    model: LogisticRegression, X_test: pd.DataFrame, instance_index: int
-):
+def get_model_from_pipeline(pipeline):
+    """
+    Extracts the logistic regression model from a scikit-learn pipeline.
+    """
+    if 'classifier' in pipeline.named_steps:
+        return pipeline.named_steps['classifier']
+    else:
+        raise ValueError("Logistic regression model not found in pipeline")
+
+
+def run_shap_analysis(model: LogisticRegression, X_test: pd.DataFrame, instance_index: int):
     """
     Runs SHAP analysis on the model for a specific instance in the test set and saves the plots to disk.
     """
@@ -37,15 +44,14 @@ def run_shap_analysis(
         shap_values = explainer(X_test)
 
         # Generate and save summary plot
+        os.makedirs("reports", exist_ok=True)
         summary_plot_path = os.path.join("reports", "shap_summary_plot.png")
         shap.summary_plot(shap_values, X_test)
         plt.savefig(summary_plot_path)
         plt.close()
 
         # Generate and save force plot for a specific instance
-        force_plot_path = os.path.join(
-            "reports", f"shap_force_plot_{instance_index}.png"
-        )
+        force_plot_path = os.path.join("reports", f"shap_force_plot_{instance_index}.png")
         shap.force_plot(
             explainer.expected_value[0],
             shap_values.values[instance_index, :],
@@ -60,12 +66,7 @@ def run_shap_analysis(
         logging.error(f"An error occurred while running SHAP analysis: {e}")
 
 
-def run_lime_analysis(
-    model: LogisticRegression,
-    X_test: pd.DataFrame,
-    y_test: pd.Series,
-    instance_index: int,
-):
+def run_lime_analysis(model: LogisticRegression, X_test: pd.DataFrame, y_test: pd.Series, instance_index: int):
     """
     Runs LIME analysis on the model for a specific instance in the test set and saves the plot to disk.
     """
@@ -87,8 +88,7 @@ def run_lime_analysis(
         )
 
         # Save the LIME plot
-        lime_plot_path = os.path.join(
-            "reports", f"lime_plot_{instance_index}.png")
+        lime_plot_path = os.path.join("reports", f"lime_plot_{instance_index}.png")
         exp.save_to_file(lime_plot_path)
 
     except Exception as e:
@@ -108,22 +108,16 @@ def extract_feature_importance(model: LogisticRegression, X: pd.DataFrame):
         ).sort_values(by="Importance", ascending=False)
 
         # Save feature importance to CSV
-        feature_importance_path = os.path.join(
-            "reports", "feature_importance.csv")
+        feature_importance_path = os.path.join("reports", "feature_importance.csv")
         feature_importance.to_csv(feature_importance_path, index=False)
 
     except Exception as e:
-        logging.error(
-            f"An error occurred while extracting feature importance: {e}")
+        logging.error(f"An error occurred while extracting feature importance: {e}")
 
 
 def main(args):
     """
     The main function for running model interpretability analysis.
-    Logs the folder with the plots and feature
-
-    Args:
-    args: Command-line arguments.
     """
 
     run = wandb.init(job_type="model_interpretability")
@@ -138,7 +132,8 @@ def main(args):
 
     # Fetching and loading the model from wandb artifacts
     model_local_path = run.use_artifact(args.model_artifact).download()
-    model = mlflow.sklearn.load_model(model_local_path)
+    pipeline = mlflow.sklearn.load_model(model_local_path)
+    model = get_model_from_pipeline(pipeline)
 
     # Run SHAP analysis
     run_shap_analysis(model, X_test, args.instance_index)
@@ -156,42 +151,19 @@ def main(args):
         description="Model interpretability reports",
     )
     reports_artifact.add_dir("reports")
+    run.log_artifact(reports_artifact)
 
     logger.info("Model interpretability analysis completed.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Run model interpretability analysis")
+    parser = argparse.ArgumentParser(description="Run model interpretability analysis")
 
-    parser.add_argument(
-        "--model_artifact",
-        type=str,
-        required=True,
-        help="The MLflow model artifact to test",
-    )
-    parser.add_argument(
-        "--X_test_artifact", type=str, required=True, help="The test features artifact"
-    )
-    parser.add_argument(
-        "--y_test_artifact", type=str, required=True, help="The test labels artifact"
-    )
-    parser.add_argument(
-        "--instance_index",
-        type=int,
-        required=True,
-        help="The index of the instance to explain",
-    )
-    parser.add_argument(
-        "--reports_artifact",
-        type=str,
-        required=True,
-        help="Name of the reports artifact to log to W&B",
-    )
+    parser.add_argument("--model_artifact", type=str, required=True, help="The MLflow model artifact to test")
+    parser.add_argument("--X_test_artifact", type=str, required=True, help="The test features artifact")
+    parser.add_argument("--y_test_artifact", type=str, required=True, help="The test labels artifact")
+    parser.add_argument("--instance_index", type=int, required=True, help="The index of the instance to explain")
+    parser.add_argument("--reports_artifact", type=str, required=True, help="Name of the reports artifact to log to W&B")
 
     args = parser.parse_args()
     main(args)
-
-
-
-    
